6ddfe0305b44bc3a5605e3bf59852a2122182d95
unknown <sebastianctsimmons@gmail.com>
2020-08-21 01:21:13 -0700

    removed unused code that didn't show up from template

--- src/App.js
+++ src/App.js
@@ -189,12 +189,6 @@ function App() {
             </div>
         </section>
         <SebFooter />
-        {/*  Scroll to Top Button (Only visible on small and extra-small screen sizes)-- */}
-        <div class="scroll-to-top d-lg-none position-fixed">
-            <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
-        </div>
-
-
     </body>
 
     </div>
